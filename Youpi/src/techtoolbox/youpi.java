529adc15654a2b3e6250ecf70c3dafc981453239
boeing23 <80139151+boeing23@users.noreply.github.com>
2025-01-01 14:02:20 -0500

    Add files via upload

---
+++ Youpi/src/techtoolbox/youpi.java
@@ -0,0 +1,19 @@
+package techtoolbox;
+
+import javax.swing.JFrame;
+
+public class youpi extends JFrame {
+	public youpi() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setLayout(null);
+		frame.setSize(600 , 600);
+		frame.setTitle("ChatBot");
+	}
+public static void main(String args[]) {
+	
+	
+}
+}
